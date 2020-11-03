820c3a8075fd1ddd2857ea7b555b039155c99c9e
Hovhannesmn <hovhannesmn@gmail.com>
2020-11-03 20:37:39 +0400

    remove redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
@@ -9,15 +9,13 @@ import './index.css';
 import Layout from './Layout';
 import applicationReducer from './store/reducers/applicationReducer';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
 const rootReducer = combineReducers({
   applicationReducer,
 });
 
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(rootReducer,
   applyMiddleware(thunk)
-));
+);
 
 const app = (
   <Provider store={store}>
